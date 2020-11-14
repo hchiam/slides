f7d296ae0d2562447d4e63314c536cc07941cd3e
hchiam <hchiam@users.noreply.github.com>
2020-11-14 15:53:13 -0500

    hide laser dot after 3s

--- other-js/laser.js
+++ other-js/laser.js
@@ -1,6 +1,12 @@
+var showLaserTimer;
 document.addEventListener("mousemove", function (e) {
   var dotSize = 10;
   var laserDot = document.getElementById("laser");
   laserDot.style.left = e.pageX - dotSize + "px";
   laserDot.style.top = e.pageY - dotSize + "px";
+  laserDot.style.display = "block";
+  clearTimeout(showLaserTimer);
+  showLaserTimer = setTimeout(function () {
+    laserDot.style.display = "none";
+  }, 3000);
 });
