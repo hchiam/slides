66c90dae761a1b6e427c8c493e84104977537716
hchiam <hchiam@users.noreply.github.com>
2020-11-15 16:41:53 -0500

    make publishing easier and cleaner

---
+++ publish.sh
@@ -0,0 +1,11 @@
+mkdir to-publish
+mkdir to-publish/other-css
+mkdir to-publish/other-js
+cp -r other-css to-publish
+cp -r other-js to-publish
+cp index.html to-publish
+cp slides.css to-publish
+cp slides.js to-publish
+
+surge to-publish https://hchiam-slides.surge.sh
+surge to-publish https://simple-slides.surge.sh
