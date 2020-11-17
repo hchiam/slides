ee688ecd27a6cab087e6bff795197961ee583f01
hchiam <hchiam@users.noreply.github.com>
2020-11-17 00:25:07 -0500

    more tests for slides, text, images

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,13 +1,4 @@
-describe("test slides", function () {
-  it("can edit text", function () {
-    cy.visit("/");
-    cy.contains("Drag this to move around. Double-click to edit text.")
-      .click()
-      .type("edited")
-      .invoke("text")
-      .should("match", /.*edited.*/i);
-  });
-
+describe("slide left/right/number", function () {
   it("can add/advance slide", function () {
     cy.visit("/");
     cy.get("#right").click();
@@ -21,12 +12,88 @@ describe("test slides", function () {
     cy.get("#slide_number").should("have.value", 1);
   });
 
+  it("can go to next/previous slide using slide number input", function () {
+    cy.visit("/");
+    cy.get("#slide_number").type("{uparrow}").should("have.value", 2);
+    cy.get("#slide_number").type("{downarrow}").should("have.value", 1);
+  });
+});
+
+describe("text", function () {
+  it("can edit text", function () {
+    cy.visit("/");
+    cy.contains("Drag this to move around. Double-click to edit text.")
+      .click()
+      .type("edited")
+      .invoke("text")
+      .should("match", /.*edited.*/i);
+  });
+
   it("can delete text", function () {
     cy.visit("/");
     cy.contains("Drag this to move around. Double-click to edit text.")
       .click()
-      .type("{selectall} {enter}");
+      .type("{selectall} "); // trigger deleting text
     cy.get("body").click();
     cy.get("p").should("not.exist");
+  });
+
+  it("cannot add new default text when default text is in default position", function () {
+    cy.visit("/");
+    cy.contains("Drag this to move around. Double-click to edit text.").should(
+      "have.length",
+      1
+    );
+    cy.get("#add_text").click();
+    cy.get("p").should("have.length", 1);
+    cy.get("#add_big_text").click();
+    cy.get("p").should("have.length", 1);
+  });
+
+  it("can add text", function () {
+    cy.visit("/");
+    cy.get("p").click().type("{selectall} "); // trigger deleting text
+    cy.get("body").click();
+    cy.get("p").should("not.exist");
+    cy.get("#add_text").click();
+    cy.get("p").should("exist");
+    cy.get("p")
+      .invoke("text")
+      .should("equal", "Drag this to move around. Double-click to edit text.");
+  });
+
+  // TODO: test dragging text (will need to check positions changed)
+});
+
+describe("image", function () {
+  it("can add image", function () {
+    cy.visit("/");
+
+    cy.get("img").should("not.exist");
+
+    cy.get("#add_image").click();
+
+    cy.fixture("cells-grid.png").then((fileContent) => {
+      cy.get("input#select_image").attachFile("cells-grid.png");
+    });
+
+    cy.get("img").should("exist");
+  });
+
+  it("can delete image", function () {
+    cy.visit("/");
+
+    cy.get("img").should("not.exist");
+
+    cy.get("#add_image").click();
+
+    cy.fixture("cells-grid.png").then((fileContent) => {
+      cy.get("input#select_image").attachFile("cells-grid.png");
+    });
+
+    cy.get("img").should("exist").dblclick();
+    cy.on("window:confirm", () => true);
+
+    cy.get("img").should("not.exist");
   });
 });
