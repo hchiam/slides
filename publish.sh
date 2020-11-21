ca851375485d4870c0587dcc7c0707179fca7635
hchiam <hchiam@users.noreply.github.com>
2020-11-20 22:46:02 -0500

    favicon (fix the favicon error message)

--- publish.sh
+++ publish.sh
@@ -3,6 +3,7 @@ mkdir to-publish/other-css
 mkdir to-publish/other-js
 cp -r other-css to-publish
 cp -r other-js to-publish
+cp favicon.png to-publish
 cp index.html to-publish
 cp slides.css to-publish
 cp slides.js to-publish
