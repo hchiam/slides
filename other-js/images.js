531148003eee8ab0119e9659c6c1cfb8113ddc53
hchiam <hchiam@users.noreply.github.com>
2020-12-27 20:34:59 -0500

    update to draggable 3.2.0 to fix #51: snap to also update position in memory

--- other-js/images.js
+++ other-js/images.js
@@ -71,6 +71,9 @@ function createImage(
       });
     }),
     snapPoints: [{ x: window.innerWidth / 2, y: window.innerHeight / 2 }],
+    snapCallback: function (left, top) {
+      updateTextPosition(img);
+    },
   });
 
   if (!isInitializingMemory) {
