7dbc0b478533c7bf4fcecbe4506d071991ebe043
hchiam <hchiam@users.noreply.github.com>
2020-12-30 16:40:47 -0500

    minify (#67)

--- bash-scripts/setup-test-site.sh
+++ bash-scripts/setup-test-site.sh
@@ -2,8 +2,15 @@ mkdir to-publish
 rm to-publish/*
 mkdir to-publish/other-css
 mkdir to-publish/other-js
-cp -r other-css to-publish
-cp -r other-js to-publish
+
+# cp -r other-css to-publish
+minify other-css/*.css > minified.css
+# cp -r other-js to-publish
+minify other-js/*.js > minified.js
+
+cp minified.css to-publish/minified.css
+cp minified.js to-publish/minified.js
+
 cp favicon.png to-publish
 cp index.html to-publish
 cp slides.css to-publish
---
+++ bash-scripts/test-locally.sh
@@ -0,0 +1,4 @@
+minify other-css/*.css > minified.css
+minify other-js/*.js > minified.js
+
+open index.html
