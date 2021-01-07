638d751c45c28a0358caf672da46829613164f4e
hchiam <hchiam@users.noreply.github.com>
2021-01-07 00:09:59 -0500

    fix #76 for now

--- bash-scripts/setup-test-site.sh
+++ bash-scripts/setup-test-site.sh
@@ -15,4 +15,16 @@ cp index.html to-publish
 cp slides.css to-publish
 cp slides.js to-publish
 
-surge to-publish https://test-slides.surge.sh/
+read -p "Are you @hchiam? (y/n) " input
+
+# TODO: change this code to detect surge permissions and run parcel locally
+
+if [[ -z "$input" ]] || [[ $input == "y" ]]
+then
+  surge to-publish https://test-slides.surge.sh
+else
+  echo
+  echo "Editing your package.json to disable the pre-commit script (commenting it out with a # character)."
+  echo
+  sed -i '' 's/"test-pre-commit":\ "/"test-pre-commit":\ "#/g' package.json
+fi
